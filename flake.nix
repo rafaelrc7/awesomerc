983c392ed021b69cf9ba1658045820f839004fcd
rafaelrc7 <rafaelrc7@gmail.com>
2022-05-01 23:39:58 -0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "My awesomewm config flake";
+
+  inputs = {
+    lain = {
+      flake = false;
+      url = "github:lcpz/lain";
+    };
+
+    awesome-freedesktop = {
+      flake = false;
+      url = "github:lcpz/awesome-freedesktop";
+    };
+  };
+
+  outputs = { lain, awesome-freedesktop, ... }: {
+    # TODO: Set xsession awesomewm
+
+    dotfiles = { config, ... }: {
+      xdg.configFile = {
+      "awesome/rc.lua".source = ./rc.lua;
+      "awesome/lua".source = ./lua;
+      "awesome/lain".source = lain;
+      "awesome/freedesktop".source = awesome-freedesktop;
+      };
+    };
+  };
+}
