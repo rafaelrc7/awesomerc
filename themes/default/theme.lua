f1b6881dd27339f149aca514d241465cc759ff97
rafaelrc7 <rafaelrc7@gmail.com>
2022-05-10 20:41:47 -0300

    Add check for default wallpaper

--- themes/default/theme.lua
+++ themes/default/theme.lua
@@ -2,6 +2,11 @@
 -- Default awesome theme --
 ---------------------------
 
+local function file_exists(name)
+    local f=io.open(name,"r")
+    if f~=nil then io.close(f) return true else return false end
+end
+
 local theme_assets = require("beautiful.theme_assets")
 local xresources = require("beautiful.xresources")
 local rnotification = require("ruled.notification")
@@ -101,7 +106,11 @@ theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar
 theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = themes_path.."default/wallpapers/spitfire.png"
+if file_exists(os.getenv("HOME") .. "/Pictures/wallpaper/default") then
+    theme.wallpaper = os.getenv("HOME") .. "/Pictures/wallpaper/default"
+else
+    theme.wallpaper = themes_path.."default/wallpapers/spitfire.png"
+end
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
